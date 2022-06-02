c1a02ba2b745f4b97de14ff0f3ef17a0087cf31f
joseph frampton <josephjamesframpton@gmail.com>
2022-06-01 20:20:16 -0600

    grab elements, run regex check to see if text has changed

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://www.nintendo.com/en-ca/store/products/nintendo-64-controller/');
 
-  await page.waitForSelector('span[data-testid="helperText"]')
-  let text = await page.$('span[data-testid="helperText"]')
-  text = (await text.getProperty('textContent')).jsonValue()
+  //await page.waitForSelector('span[data-testid="helperText"]')
+  //let text = await page.$('span[data-testid="helperText"]')
+  //text = (await text.getProperty('textContent')).jsonValue()
+  //console.log(text)
+
+  
+  let searchText = 'p.cWuGKg.iqhwUx'
+  await page.waitForSelector(searchText)
+  let text = await page.$(searchText)
+  text = await text.getProperty('innerHTML')
+  text = await text.jsonValue()
   console.log(text)
 
-  await page.waitForSelector('button.goRNPN')
-  let button = await page.$('button.goRNPN')
-  let inner = await button.getProperty('innerHTML')
-  inner = await inner.jsonValue()
-  console.log(inner)
+  const textRegex = new RegExp('This item is currently unavailable');
+  console.log(textRegex.test(text))
+
+  let searchButton = 'button.goRNPN'
+  await page.waitForSelector(searchButton)
+  let button = await page.$(searchButton)
+  button = await button.getProperty('innerHTML')
+  button = await button.jsonValue()
+  console.log(button)
+
+  const buttonRegex = new RegExp('Sold out');
+  console.log(buttonRegex.test(button))
 
   await page.screenshot({ path: 'n64-controller.png' });
 
